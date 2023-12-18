c3d204a63182c63a7b5bc65509c28d65d37732f0
Al <sagarinoclarin@gmail.com>
2023-12-18 12:40:11 +0800

    feat: add turnstile and updated create cloud account form

---
+++ _includes/js/actions.js
@@ -0,0 +1,53 @@
+function createCloudUser(event) {
+    event.preventDefault();
+
+    const submitButton = document.getElementById("cloud-sign-up-form-submit")
+    const errorElement = document.getElementById("cloud-sign-up-error");
+    const errorMessageElement = document.getElementById("cloud-sign-up-error-message");
+
+    errorElement.style.display = "none"
+
+    const formData = new FormData(event.target);
+    let formObj = {};
+    
+    for (const [fieldName] of formData) {
+        const fieldValue = formData.getAll(fieldName);
+        formObj[fieldName] = fieldValue.length == 1 ? fieldValue.toString() : fieldValue
+    }
+
+    const parameters = {
+        firstname: formObj['1_first_name'],
+        lastname: formObj['2_last_name'],
+        email: formObj['3_email'],
+        password2: formObj['4_password'],
+        turnstileResponse: formObj['cf-turnstile-response']
+    }
+
+    const serverUrl = 'http://localhost/whmcs/cloudsignup.php';
+
+    submitButton.disabled = true
+    submitButton.innerHTML = "Submitting..."
+    fetch(serverUrl, {
+        method: 'POST',
+        body: JSON.stringify(parameters),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then(res => {
+        return res.json()
+    }).then(res => {
+        const { data } = res
+        if(data?.result == 'success' && data?.redirect_url) {
+            submitButton.innerHTML = "Success!"
+            window.location.replace(data.redirect_url);
+        }else if(data?.result == 'error') {
+            submitButton.disabled = false
+            submitButton.innerHTML = "Let’s go"
+            errorElement.style.display = "block"
+            errorMessageElement.innerHTML = data.message
+        }
+    }).catch(error => {
+         submitButton.disabled = false
+         console.error(error) 
+    })
+}
