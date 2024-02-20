6886bae68c93554410268d8aea784a978cb4174f
Al <sagarinoclarin@gmail.com>
2024-02-20 12:27:09 +0800

    feat: us AF as default country

--- _includes/js/actions.js
+++ _includes/js/actions.js
@@ -22,52 +22,54 @@ async function createCloudUser(event) {
   submitButton.innerHTML = "Submitting...";
   const widgetId = "#cf-widget";
 
+  let country;
   try {
     const ipInfo = await fetch(`https://ipinfo.io/json?token=9a0c888a083a9e`);
     const ipInfoJson = await ipInfo.json();
 
-    const parameters = {
-      firstname: formObj["1_first_name"],
-      lastname: formObj["2_last_name"],
-      email: formObj["3_email"],
-      password2: formObj["4_password"],
-      turnstileResponse: formObj["cf-turnstile-response"],
-      country: ipInfoJson.country,
-    };
-
-    fetch(serverUrl, {
-      method: "POST",
-      body: JSON.stringify(parameters),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        const { data } = res;
-        if (data?.result == "success" && data?.redirect_url) {
-          submitButton.innerHTML = "Success!";
-          window.location.replace(data.redirect_url);
-        } else if (data?.result == "error") {
-          submitButton.disabled = false;
-          submitButton.classList.remove("btn-primary-loading");
-          submitButton.innerHTML = "Let’s go";
-          errorElement.style.display = "block";
-          errorMessageElement.innerHTML = data.message;
-          turnstile.reset(widgetId);
-        }
-      })
-      .catch((error) => {
-        submitButton.disabled = false;
-        submitButton.classList.remove("btn-primary-loading");
-        console.error(error);
-        turnstile.reset(widgetId);
-      });
+    country = ipInfoJson.country || "AF";
   } catch (error) {
     console.error(error);
-    submitButton.disabled = false;
-    submitButton.classList.remove("btn-primary-loading");
+    country = "AF";
   }
+
+  const parameters = {
+    firstname: formObj["1_first_name"],
+    lastname: formObj["2_last_name"],
+    email: formObj["3_email"],
+    password2: formObj["4_password"],
+    turnstileResponse: formObj["cf-turnstile-response"],
+    country: country,
+  };
+
+  fetch(serverUrl, {
+    method: "POST",
+    body: JSON.stringify(parameters),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      const { data } = res;
+      if (data?.result == "success" && data?.redirect_url) {
+        submitButton.innerHTML = "Success!";
+        window.location.replace(data.redirect_url);
+      } else if (data?.result == "error") {
+        submitButton.disabled = false;
+        submitButton.classList.remove("btn-primary-loading");
+        submitButton.innerHTML = "Let’s go";
+        errorElement.style.display = "block";
+        errorMessageElement.innerHTML = data.message;
+        turnstile.reset(widgetId);
+      }
+    })
+    .catch((error) => {
+      submitButton.disabled = false;
+      submitButton.classList.remove("btn-primary-loading");
+      console.error(error);
+      turnstile.reset(widgetId);
+    });
 }
